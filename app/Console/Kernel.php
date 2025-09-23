6a5fae3fa6f9e87e8657520ccbab20296f747b99
Polyshinnel <polyshinnel@gmail.com>
2025-09-23 12:28:45 +0300

    Изменен интервал выполнения команды изменения размера изображений товаров с каждых 20 минут на каждые 5 минут для повышения эффективности обработки изображений.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,9 +13,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        
+
         // Выполнение команды изменения размера изображений товаров каждые 20 минут
-        $schedule->command('images:resize-products')->everyTwentyMinutes();
+        $schedule->command('images:resize-products')->everyFiveMinutes();
     }
 
     /**
